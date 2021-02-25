64395e8ed1276b587adf42ca3d50f10dd13a4f37
Fernando Guevara <luisfernandoguevara95@gmail.com>
2021-02-24 21:22:41 -0600

    (Add): Auth Context

---
+++ src/auth/AuthContext.js
@@ -0,0 +1,43 @@
+import { createContext, useCallback, useState } from "react";
+
+export const AuthContext = createContext();
+
+const initialState = {
+  uid: null,
+  checking: true,
+  logged: false,
+  name: null,
+  email: null
+};
+
+export const AuthProvider = ({children}) => {
+
+  const [auth, setAuth] = useState(initialState);
+
+  const login = (email, password) => {
+
+  };
+
+  const register = (name, email, password) => {
+
+  };
+
+  const checkToken = useCallback(() =>{}, []);
+
+  const logout = () => {
+
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+          login,
+          register,
+          checkToken,
+          logout,
+      }}
+    >
+      { children }
+    </AuthContext.Provider>
+  );
+};
