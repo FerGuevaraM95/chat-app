98283ba16ef41ea087b56675555e3856588d6dc9
Fernando Guevara <luisfernandoguevara95@gmail.com>
2021-02-27 17:36:00 -0600

    (Add): Public and private routes

---
+++ src/router/PublicRoute.js
@@ -0,0 +1,17 @@
+import { Route, Redirect } from "react-router-dom"
+
+export const PublicRoute = ({
+  isAuthenticated,
+  component: Component,
+  ...rest
+}) => {
+  return (
+    <Route {...rest}
+      component={(props) => (
+        ( !isAuthenticated )
+          ? <Component {...props} />
+          : <Redirect to="/" />
+      )}
+      />
+  );
+};
